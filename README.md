6c10d9daa781d0d92a6a2a59192514dd3ecc49c6
Dario Giovannetti <dev@dariogiovannetti.net>
2016-09-26 15:17:22 +0800

    Update the package name everywhere

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# lib.py.html
+# Htool
 
-Python module to create and manipulate HTML documents using Python syntax
-and object-oriented programming.
+HyperText Object-Oriented Layer: create and manipulate HTML documents using
+Python objects.
 
 ## License
 
-lib.py.html is distributed under the terms of the
+Htool is distributed under the terms of the
 [GNU General Public License v3.0](http://www.gnu.org/copyleft/gpl.html)
 (see LICENSE).
--- htool/__init__.py
+++ htool/__init__.py
@@ -1,20 +1,20 @@
-# lib.py.htool - Check the status of code repositories under a root directory.
+# Htool - HyperText Object-Oriented Layer.
 # Copyright (C) 2016 Dario Giovannetti <dev@dariogiovannetti.net>
 #
-# This file is part of lib.py.htool.
+# This file is part of Htool.
 #
-# lib.py.htool is free software: you can redistribute it and/or modify
+# Htool is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# lib.py.htool is distributed in the hope that it will be useful,
+# Htool is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with lib.py.htool.  If not, see <http://www.gnu.org/licenses/>.
+# along with Htool.  If not, see <http://www.gnu.org/licenses/>.
 
 # It shouldn't be necessary to expose the dom module
 from . import docs, text
